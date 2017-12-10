63d4acc21ea451f0df21df5585574394efbba1bb
Jasper Bok <hello@jasperbok.nl>
2017-12-10 14:37:07 +0100

    Adds the Project struct and GetProject API method

---
+++ project.go
@@ -0,0 +1,40 @@
+package jira
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+)
+
+type Project struct {
+	Id          string
+	Key         string
+	Description string
+	Name        string
+	Lead        User
+}
+
+func (p *Project) LeadDisplayName() string {
+	return p.Lead.DisplayName
+}
+
+func (p *Project) Print() {
+	fmt.Printf("%s (%s)", p.Name, p.Key)
+}
+
+// GetProject gets a project's information and returns it as a Project.
+func GetProject(keyOrId string) (p Project, err error) {
+	status, res, err := callApi("GET", "/project/"+keyOrId, nil)
+
+	if err != nil {
+		return
+	}
+
+	if status == 404 {
+		return p, errors.New("404")
+	}
+
+	err = json.Unmarshal(res, &p)
+
+	return
+}
